44ef8c7e80e9a828165a19df8aefedcf903bdc0c
keisuke sato <corydoras121@gmail.com>
2023-01-11 23:16:15 +0900

    [update]classに変更

--- src/js/main.js
+++ src/js/main.js
@@ -4,124 +4,156 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import vertexSource from "./shader/vertexShader.glsl?raw";
 import fragmentSource from "./shader/fragmentShader.glsl?raw";
 
-// const img = require("../images/image.jpg");
 import img from '../images/image.jpg';
 
-let renderer, scene, camera, fovRadian, distance, geometry, mesh;
+class Main {
+  constructor() {
+    this.viewport = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
 
-const canvas = document.querySelector("#canvas");
+    this.canvas = document.querySelector("#canvas");
+    this.renderer = null;
+    this.scene = new THREE.Scene();
+    this.camera = null;
+    this.cameraFov = 45;
+    this.cameraFovRadian = (this.cameraFov / 2) * (Math.PI / 180);
+    this.cameraDistance = (this.viewport.height / 2) / Math.tan(this.cameraFovRadian);
+    this.controls = null;
+    this.geometry = null;
+    this.material = null;
+    this.mesh = null;
 
-let size = {
-  width: window.innerWidth,
-  height: window.innerHeight
-};
+    this.viewport = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
 
-async function init(){
+    this.uniforms = {
+      uTime: {
+        value: 0.0
+      },
+      uTex: {
+        value: this.texture
+      },
+      uResolution: {
+        value: new THREE.Vector2(this.viewport.width, this.viewport.height)
+      },
+      uTexResolution: {
+        value: new THREE.Vector2(2048, 1024)
+      },
+    };
 
-  // レンダラー
-  renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(size.width, size.height);
-
-  //シーン
-  scene = new THREE.Scene();
-
-  //カメラ
-  //ウインドウとWebGL座標を一致させる
-  const fov = 45;
-  fovRadian = (fov / 2) * (Math.PI / 180); //視野角をラジアンに変換
-  distance = (size.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
-  camera = new THREE.PerspectiveCamera(fov, size.width / size.height, 1, distance * 2);
-  camera.position.z = distance;
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-  scene.add(camera);
-
-  //コントローラー
-  const controls = new OrbitControls(camera, canvas);
-  controls.enableDamping = true;
-
-  //ジオメトリ
-  geometry = new THREE.PlaneGeometry(size.width, size.height, 40, 40);
-
-  //テクスチャ
-  const loader = new THREE.TextureLoader();
-  const texture = await loader.loadAsync(img);
-
-  //GLSL用データ
-  let uniforms = {
-    uTime: {
-      value: 0.0
-    },
-    uTex: {
-      value: texture
-    },
-    uResolution: {
-      value: new THREE.Vector2(size.width, size.height)
-    },
-    uTexResolution: {
-      value: new THREE.Vector2(2048, 1024)
-    },
-  };
-
-  //マテリアル
-  const material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    vertexShader: vertexSource,
-    fragmentShader: fragmentSource,
-    side: THREE.DoubleSide
-  });
-
-  //メッシュ
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-
-  function animate(){
-    //アニメーション処理
-
-    uniforms.uTime.value += 0.03;
-    
-    //レンダリング
-    renderer.render(scene, camera);
-    controls.update();
-    requestAnimationFrame(animate);
+    this.init();
+    // this._init();
+    // this._update();
+    // this._addEvent();
   }
-  animate();
-  
+
+  _setRenderer() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
+  }
+
+  _setCamera() {
+    // this.camera = new THREE.PerspectiveCamera(45, this.viewport.width / this.viewport.height, 1, 100);
+    // this.camera.position.set(0, 0, 5);
+    // this.scene.add(this.camera);
+
+    //ウインドウとWebGL座標を一致させる
+    // const fov = 45;
+    // const fovRadian = (fov / 2) * (Math.PI / 180); //視野角をラジアンに変換
+    // const distance = (this.viewport.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
+    this.camera = new THREE.PerspectiveCamera(this.cameraFov, this.viewport.width / this.viewport.height, 1, this.cameraDistance * 2);
+    this.camera.position.z = this.cameraDistance;
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+    this.scene.add(this.camera);
+  }
+
+  _setControlls() {
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.controls.enableDamping = true;
+  }
+
+  _setLight() {
+    const light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(1, 1, 1);
+    this.scene.add(light);
+  }
+
+  _addMesh() {
+    //ジオメトリ
+    this.geometry = new THREE.PlaneGeometry(this.viewport.width, this.viewport.height, 40, 40);
+
+    //テクスチャ
+    const loader = new THREE.TextureLoader();
+    this.uniforms.uTex.value = loader.load(img);
+
+    console.log(this.texture);
+
+    //マテリアル
+    this.material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: vertexSource,
+      fragmentShader: fragmentSource,
+      side: THREE.DoubleSide
+    });
+
+    //メッシュ
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+  init() {
+    this._setRenderer();
+    this._setCamera();
+    this._setControlls();
+    this._setLight();
+    this._addMesh();
+
+    this._update();
+    this._addEvent();
+  }
+
+  _update() {
+
+    this.uniforms.uTime.value += 0.03;
+
+    //レンダリング
+    this.renderer.render(this.scene, this.camera);
+    this.controls.update();
+    requestAnimationFrame(this._update.bind(this));
+  }
+
+  _onResize() {
+    this.viewport = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+    // レンダラーのサイズを修正
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
+    // カメラのアスペクト比を修正
+    this.camera.aspect = this.viewport.width / this.viewport.height;
+    this.camera.updateProjectionMatrix();
+    // カメラの位置を調整
+    this.cameraDistance = (this.viewport.height / 2) / Math.tan(this.cameraFovRadian); //ウインドウぴったりのカメラ距離
+    this.camera.position.z = this.cameraDistance;
+    // uniforms変数に反映
+    this.mesh.material.uniforms.uResolution.value.set(this.viewport.width, this.viewport.height);
+    // meshのscale設定
+    const scaleX = Math.round(this.viewport.width / this.mesh.geometry.parameters.width * 100) / 100 + 0.01;
+    const scaleY = Math.round(this.viewport.height / this.mesh.geometry.parameters.height * 100) / 100 + 0.01;
+    this.mesh.scale.set(scaleX, scaleY, 1);
+  }
+
+  _addEvent() {
+    window.addEventListener("resize", this._onResize.bind(this));
+  }
 }
 
-init();
-
-// ラジアンに変換
-// function radian(val) {
-//   return (val * Math.PI) / 180;
-// }
-
-// ランダムな数
-// function random(min, max) {
-//   return Math.random() * (max - min) + min;
-// }
-
-//リサイズ
-function onWindowResize() {
-  size.width = window.innerWidth;
-  size.height = window.innerHeight;
-  // レンダラーのサイズを修正
-  renderer.setSize(size.width, size.height);
-  // カメラのアスペクト比を修正
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
-  // カメラの位置を調整
-  distance = (size.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
-  camera.position.z = distance;
-  // uniforms変数に反映
-  mesh.material.uniforms.uResolution.value.set(size.width, size.height);
-  // meshのscale設定
-  const scaleX = Math.round(size.width / mesh.geometry.parameters.width * 100) / 100 + 0.01;
-  const scaleY = Math.round(size.height / mesh.geometry.parameters.height * 100) / 100 + 0.01;
-  mesh.scale.set(scaleX, scaleY, 1);
-}
-window.addEventListener("resize", onWindowResize);
+const main = new Main();
